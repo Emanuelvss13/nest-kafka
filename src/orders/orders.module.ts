8c048a1610873bded622314a635a607ec3df94a5
Emanuelvss13 <emanuelsouza2002@gmail.com>
2023-02-15 14:40:27 -0300

    project nest with kafka

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { ClientKafka, ClientsModule, Transport } from '@nestjs/microservices';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Order } from './entities/order.entity';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+
+@Module({
+  imports: [
+    SequelizeModule.forFeature([Order]),
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['kafka:9092'],
+          },
+          consumer: {
+            groupId: 'my-group-producer',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [OrdersController],
+  providers: [
+    OrdersService,
+    {
+      provide: 'KAFKA_PRODUCER',
+      useFactory: async (kafkaService: ClientKafka) => {
+        return kafkaService.connect();
+      },
+      inject: ['KAFKA_SERVICE'],
+    },
+  ],
+})
+export class OrdersModule {}
